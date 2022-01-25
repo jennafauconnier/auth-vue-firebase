07b8c238b5db148f2064077f179087e04a10225a
Jenna Diop <j.diop@fiters.co>
2022-01-25 20:25:13 +0100

    add css authentication

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <div id="nav">
+  <div id="nav" v-if="$store.state.user">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <button @click="$store.dispatch('logout')" >Logout</button>
   </div>
   <router-view/>
 </template>
+
+<script>
+import { onBeforeMount } from 'vue'
+import { useStore } from 'vuex'
+export default {
+  setup() {
+    const store = useStore()
+    onBeforeMount(() => {
+      store.dispatch('fetchUser')
+    })
+  }
+}
+</script>
 
 <style>
 * {
